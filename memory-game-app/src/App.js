9986762f3d3cd97fb2c9d8ee458b6e5ae8e109ce
Miguel <migijc@gmail.com>
2022-10-08 03:25:06 -0400

    Basic components and logic added. Also a simple UI for now

---
+++ memory-game-app/src/App.js
@@ -0,0 +1,15 @@
+import CardsDisplay from "./components/CardsDisplay";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="cardDisplay">
+       <CardsDisplay/>
+    
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ memory-game-app/src/components/Card.js
@@ -0,0 +1,14 @@
+import React, {useEffect} from 'react'
+export default function Card(props){
+
+  
+
+    return(
+        <div className="displayedCard">
+            <p>{props.name}</p>
+            <div className="cardImage" style={{backgroundImage:(`url(${props.source})`)}}/>
+        </div>
+    )
+}
+
+
